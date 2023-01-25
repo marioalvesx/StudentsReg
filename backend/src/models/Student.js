d7b811136293820989d62935e9ef2d3f50e0e8bf
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-01-25 02:40:59 -0300

    Delete old files

--- backend/src/models/Student.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const StudentSchema = new mongoose.Schema({    // Tela para login para cadastro de estudante
-    matricula: Number
-});
-
-module.exports = mongoose.model('Student', StudentSchema);
