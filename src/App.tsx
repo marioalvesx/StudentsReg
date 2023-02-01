cd105d1e8feaac4ab8337273eac83ec7068ee2e3
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-01-31 22:31:24 -0300

    Add complete, resume and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
-import { useState } from 'react'
-import Student from './components/Student'
+import Student from './components/Student/index'
 import './App.css'
+import { ChangeEvent, useEffect, useState } from 'react'
+
+export type StudentTodo = {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 function App() {
+  const [studentTodoInput, setTodoInput] = useState('');
+  const [studentsTodos, setStudentsTodos] = useState<StudentTodo[]>(() => {
+    const storedTodos = localStorage.getItem('@studentList:studentsTodos');
+
+    if (storedTodos) {
+      return JSON.parse(storedTodos);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@studentList:studentsTodos', JSON.stringify(studentsTodos))
+  }, [studentsTodos])
+
+  function addStudentTodo() {
+    setStudentsTodos((previousTodos) => 
+      [...previousTodos, { id: Math.random(), title: studentTodoInput, completed: false }]
+    );
+    setTodoInput('');
+  }
+
+  function completeTodo(id: number) {
+    setStudentsTodos((previousTodos) => 
+      previousTodos.map((todo) => todo.id !== id ? todo : { ...todo, completed: !todo.completed })
+    );
+  }
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    setTodoInput(e.target.value);
+  }
+
+  function deleteTodo(id: number) {
+    setStudentsTodos((previousTodos) => previousTodos.filter((studentsTodos) => studentsTodos.id !== id));
+  }
+
   return (
     <div className="App">
+      <h1>📖 Student Register</h1>
+      <br /><hr /><br />
       <div className="add-student">
-        <input placeholder="Student Name" />
-        <button>Add</button>
+        <input placeholder="Student Name" value={studentTodoInput} onChange={handleInputChange}/>
+        <button onClick={addStudentTodo}>Add</button>
       </div>
-
-      <Student title="Student name" />
+      {
+        studentsTodos.map((student) => (
+          <Student key={student.id} studentTodo={student} completeTodo={completeTodo} deleteTodo={deleteTodo}/>
+        ))
+      }
     </div>
   )
 }
--- src/components/Student/index.tsx
+++ src/components/Student/index.tsx
@@ -1,9 +1,30 @@
+import { StudentTodo } from '../../App'
+import './styles.css'
+
 type StudentProps = {
-  title: string;
+  studentTodo: StudentTodo;
+  completeTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
 }
 
-export default function Student({ title }: StudentProps ) {
+export default function Student({ studentTodo, completeTodo, deleteTodo }: StudentProps ) {
+  function handleCompleteTodo() {
+    completeTodo(studentTodo.id)
+  }
+  
+  function handleDeleteTodo() {
+    deleteTodo(studentTodo.id)
+  }
+
   return (
-    <div>{ title }</div>
+    <div className={`student ${studentTodo.completed ? 'done' : ''}`}>
+      <h2>{studentTodo.title}</h2>
+
+      <div className="student-buttons">
+        <button onClick={handleCompleteTodo}>
+          {studentTodo.completed ? 'Resume ✏️' : 'Complete ✅ '}</button>
+        <button onClick={handleDeleteTodo}>Delete ❌ </button>
+      </div>
+    </div>
   )
 }
